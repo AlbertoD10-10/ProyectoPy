5c7de8630679362f89a3478cea1a7863fa3ef47e
sgomeza13 <sgomeza13@eafit.edu.co>
2023-05-31 20:18:11 -0500

    deleted usless files, final commit

--- audiomixer2.py
+++ audiomixer2.py
@@ -1,14 +1,18 @@
 from pydub import AudioSegment
 import multiprocessing as mp
-import os
 import time
 import sys
+import os
 
 def leer_cancion(cancion_path):
+
     cancion = AudioSegment.from_mp3(cancion_path)
+
     return cancion
 
-def combinar_canciones(canciones, name):
+def combinar_canciones(canciones,name):
+    start = time.time()
+    
     # Combinar las canciones
     listaCanciones = list(canciones)
     if(listaCanciones[0].duration_seconds > listaCanciones[1].duration_seconds):   #Es mas rapido usar .duration_seconds que len()
@@ -22,69 +26,54 @@ def combinar_canciones(canciones, name):
 
     # Guardar la canción combinada en un archivo MP3
     cancion_combinada.export(name, format='mp3')
+    print("combinacion exitosa")
+    end = time.time()
+    print("Las canciones se combinaron en(s):", end - start)
 
-    print("¡Las canciones se han combinado correctamente!")
 
 
-
-def paralelo(file_1, file_2, name):
+def paralelo(file1,file2,name):
     
-    start = time.time()
-
-    #path_1 = "files/"+file_1
-    #path_2 = "files/"+file_2
-
+    
     # Obtener la lista de rutas de las canciones
     canciones_paths = [
-        file_1,
-        file_2
+        file1,
+        file2,
     ]
 
 
-     # Cantidad de cores de la máquina
-    cores = os.cpu_count()
-    print("Su máquina tiene", cores, "cores.")
 
     # Crear el objeto Pool con la cantidad de procesos deseados
     with mp.Pool() as pool:
 
     # Leer las canciones en paralelo utilizando pool.map()
         canciones = pool.imap_unordered(leer_cancion, canciones_paths)
-        print("¡Las lecturas se han ejecutado en paralelo!")
+        print("Las lecturas se han ejecutado en paralelo")
         pool.close()
         pool.join()
+
     # Combinar las canciones
-        #combinacion = pool.imap_unordered(combinar_canciones, canciones)
-    res = mp.Process(target=combinar_canciones, args = (canciones, name))
+        #pool.imap(combinar_canciones, canciones)
+        #combinar_canciones(canciones)
+    res = mp.Process(target=combinar_canciones,args=(canciones,name))
     res.run()
-    end = time.time()
-    print("\nLos archivos se leyeron en(s):", end - start)
 
-
-    
-
-def secuencial(file_1, file_2, name):
+def secuencial(file1,file2,name):
     start = time.time()
+    canciones_paths = [
+        file1,
+        file2
+    ]
+    audio1 = leer_cancion(canciones_paths[0])
+    audio2 = leer_cancion(canciones_paths[1])
 
-    #path_1 = "files/"+file_1
-    #path_2 = "files/"+file_2
-
-    #canciones_paths = [
-    #    "files/Duvet.mp3",
-    #    "files/UN_OWEN_WAS_HER.mp3"
-    #]
-    audio1 = leer_cancion(file_1)
-    audio2 = leer_cancion(file_2)
-    #audio3 = leer_cancion(canciones_paths[2])
-    #audio4 = leer_cancion(canciones_paths[3])
-    print("¡Las lecturas se han ejecutado de manera secuencial!")
-    canciones = [audio1, audio2]
-    combinar_canciones(canciones, name)
+    canciones = [audio1,audio2]
+    combinar_canciones(canciones,name)
     end = time.time()
     print("\nEl archivo se leyo en(s):", end - start)
 
-
 def main():
+    start = time.time()
     # Recuperar los argumentos de línea de comandos
     argumentos = sys.argv
     print(argumentos)
@@ -104,13 +93,9 @@ def main():
             paralelo(argumentos[1], argumentos[2], argumentos[3])
         elif len(argumentos) == 3:
             paralelo(argumentos[1], argumentos[2], "audio_mixer.mp3")
-
-        
+    end = time.time()
+    print("El tiempo total de ejecucion es(s):", end - start)
 
 
 if __name__ == '__main__':
     main()
-
-
-
-   
